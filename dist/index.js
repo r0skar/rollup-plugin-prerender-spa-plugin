d3b9997ab5b707b02c1692f8ce5cec9b17cb2941
Oskar Rainer <rainer.oskar@gmail.com>
2019-05-20 17:23:46 +0200

    add pupeteer and prerenderer config

--- dist/index.js
+++ dist/index.js
@@ -2,26 +2,26 @@
 
 var fs = require('fs');
 var path = require('path');
+var util = require('util');
 var Prerenderer = require('@prerenderer/prerenderer');
 var Puppeteer = require('@prerenderer/renderer-puppeteer');
-var util = require('util');
-
-const DIST_FOLDER = '/home/oskar/dev/selbststaendig.jetzt/dist';
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const startPrerenderer = async (options) => {
-  if (!options.routes || options.routes.length === 0) throw Error('`routes` must be of type string[]')
+  const { routes, puppeteer = {}, ...config } = options;
 
-  const renderer = new Puppeteer(options.inject ? { inject: options.inject } : undefined);
-  const prerenderer = new Prerenderer({ staticDir: DIST_FOLDER, renderer });
+  if (!routes || routes.length === 0) throw Error('`routes` must be of type `string[]`')
+
+  const renderer = new Puppeteer(puppeteer);
+  const prerenderer = new Prerenderer({ renderer, ...config });
 
   await prerenderer.initialize();
-  const rendered = await prerenderer.renderRoutes(options.routes);
+  const rendered = await prerenderer.renderRoutes(routes);
 
   for (const route of rendered) {
     try {
-      const out = path.join(DIST_FOLDER, route.route);
+      const out = path.join(config.staticDir, route.route);
       const file = path.normalize(`${out}/index.html`);
       console.info(`Rendering route "${route.route}"...`);
       await writeFileAsync(file, route.html);
@@ -33,8 +33,8 @@ const startPrerenderer = async (options) => {
   prerenderer.destroy();
 };
 
-const rollupPrerenderer = (options = {}) => ({
-  name: 'prerenderer',
+const rollupSpaPrerenderer = (options = {}) => ({
+  name: 'spaPrerenderer',
   writeBundle: () =>  {
     try {
       startPrerenderer(options);
@@ -44,4 +44,4 @@ const rollupPrerenderer = (options = {}) => ({
   }
 });
 
-module.exports = rollupPrerenderer;
+module.exports = rollupSpaPrerenderer;
