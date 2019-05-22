beadc9dd52732dad951c231f505e64c14967c04d
Oskar Rainer <rainer.oskar@gmail.com>
2019-05-22 10:07:04 +0200

    add types

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,37 @@ export = prerendererPlugin
 
 declare function prerendererPlugin (options?: PrerendererConfig): import('rollup').Plugin
 
-interface PrerendererConfig {}
+export interface PrerendererConfig {
+  staticDir: string
+  routes: string[]
+  indexPath?: string
+  renderer?: PuppeteerConfig
+}
+
+interface PuppeteerConfig {
+  injectProperty?: string
+  inject?: { [key: string]: unknown }
+  ignoreHTTPSErrors?: boolean
+  headless?: boolean
+  executablePath?: string
+  slowMo?: number
+  defaultViewport?: {
+    width?: number
+    height?: number
+    deviceScaleFactor?: number
+    isMobile?: boolean
+    hasTouch?: boolean
+    isLandscape?: boolean
+  }
+  args?: string[]
+  ignoreDefaultArgs?: boolean | string[]
+  handleSIGINT?: boolean
+  handleSIGTERM?: boolean
+  handleSIGHUP?: boolean
+  timeout?: number
+  dumpio?: boolean
+  userDataDir?: string
+  env?: { [key: string]: unknown }
+  devtools?: boolean
+  pipe?: boolean
+}
