5edc80c25371d077ded60072625bac9378f8afe2
Oskar Rainer <rainer.oskar@gmail.com>
2019-05-22 10:12:18 +0200

    change pkg name

--- dist/index.js
+++ dist/index.js
@@ -9,24 +9,26 @@ var Puppeteer = require('@prerenderer/renderer-puppeteer');
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const startPrerenderer = async (options) => {
-  const { routes, puppeteer = {}, ...config } = options;
+  const { routes, renderer = {}, ...config } = options;
+
+  if (!config.staticDir) throw Error('`staticDir` must be of type `string`')
 
   if (!routes || routes.length === 0) throw Error('`routes` must be of type `string[]`')
 
-  const renderer = new Puppeteer(puppeteer);
-  const prerenderer = new Prerenderer({ renderer, ...config });
+  const puppeteer = new Puppeteer(renderer);
+  const prerenderer = new Prerenderer({ renderer: puppeteer, ...config });
 
   await prerenderer.initialize();
   const rendered = await prerenderer.renderRoutes(routes);
 
   for (const route of rendered) {
     try {
-      const out = path.join(config.staticDir, route.route);
+      const out = path.join(staticDir, route.route);
       const file = path.normalize(`${out}/index.html`);
       console.info(`Rendering route "${route.route}"...`);
       await writeFileAsync(file, route.html);
     } catch (e) {
-      console.warn(`Skipping route "${route.route}"`, e);
+      console.warn(e);
     }
   }
 
